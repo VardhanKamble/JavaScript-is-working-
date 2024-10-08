4f171ae6094bfbdc66b6fd8ee3aaf8f31139ed7c
Vardhan Kamble <158852207+VardhanKamble@users.noreply.github.com>
2024-10-09 00:10:29 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("changeTextBtn").addEventListener("click", function() {
+    const message = document.getElementById("message");
+    message.textContent = "You click the button! JavaScript is working!";
+});
